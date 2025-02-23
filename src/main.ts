417f31be163c08fdaf6b57234d26c1b4343dd08f
unknown <ystar5008@naver.com>
2025-02-23 17:11:57 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3001, () => {
+    console.log('실행');
+  });
 }
 bootstrap();
